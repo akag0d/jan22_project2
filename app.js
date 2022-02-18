68ff9456b87aeaa021072fd893ae922e92c39208
Luciana A <lucianaalbuquerque12@gmail.com>
2022-02-18 15:11:15 +0000

    adds spotify auth

--- app.js
+++ app.js
@@ -30,6 +30,9 @@ app.use("/", index);
 const authRoutes = require("./routes/auth");
 app.use("/auth", authRoutes);
 
+const spotifyAuth = require("./routes/spotify-auth");
+app.use("/", spotifyAuth);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
