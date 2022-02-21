a9ff08cf29132a6cfc1d984c039f0a8350c02d26
Luciana A <lucianaalbuquerque12@gmail.com>
2022-02-21 17:56:30 +0000

    adds keeping a token in applocals

--- app.js
+++ app.js
@@ -21,6 +21,11 @@ require("./config")(app);
 const projectName = "project2";
 const capitalized = (string) => string[0].toUpperCase() + string.slice(1).toLowerCase();
 
+app.use(function(req,res,next) {
+    console.log('APP LOCALS', app.locals)
+    next()
+}) 
+
 app.locals.title = `${capitalized(projectName)} created with IronLauncher`;
 
 // 👇 Start handling routes here
--- routes/spotify.routes.js
+++ routes/spotify.routes.js
@@ -4,18 +4,19 @@ const User = require("../models/User.model");
 const axios = require('axios');
 const urlParams = require('url-search-params')
 const querystring = require('querystring');
+const isLoggedIn = require("../middleware/isLoggedIn");
 
-const baseUrl = 'https://api.spotify.com/v1'
 
-router.get('/create/access_token=:accessToken&refresh_token=:refreshToken', (req,res,next) => {
+  router.get('/create/access_token=:accessToken&refresh_token=:refreshToken', (req,res,next) => {
+    req.app.locals.accessToken = req.params.accessToken
+    req.app.locals.refreshToken = req.params.refreshToken
     
-    const accessToken = req.params.accessToken;
-    const refreshToken = req.params.refreshToken
+    res.render('list/create-playlist')
+  })
 
-    console.log('access', req.params.accessToken, 'refresh: ', refreshToken)
-    res.render('profile')
-
-    return {accessToken, refreshToken}
-})
+  router.post('/create/logout', isLoggedIn, (req,res,next) =>{
+    app.locals.accessToken = ''
+    res.redirect('/')
+  })
 
   module.exports = router;
