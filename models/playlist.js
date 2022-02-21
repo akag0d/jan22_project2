45b532ee909ee112918d3badd1e8ee09de667044
akag0d <ruben.neves90@gmail.com>
2022-02-21 13:02:14 +0000

    alt

---
+++ models/playlist.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const playlistSchema = new Schema ({
+    name: String,
+},)
---
+++ routes/db.routes.js
@@ -0,0 +1,44 @@
+const express = require('express');
+
+const tracksRouter = express.Router(),
+
+const
+
+
+
+
+
+
+
+
+
+/*require('dotenv').config();
+
+const express = require('express');
+const hbs = require('hbs');
+
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const app = express();
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+});
+
+
+//THIS WILL RETRIEVE AN ACCESS TOKEN
+spotifyApi
+.clientCredentialsGrant()
+.then(data => spotifyApi.setAccessToken(data.body['access_token']))
+.catch(error => console.log('Something went wrong when retrieving an access token', error));
+
+app.get('/', (req, res, next) => {
+    res.render('index');
+});
+
+app.get()
